d4c30529277fa626a90aea7ac45d3229a1f98641
YuanWANG2662 <82698716+YuanWANG2662@users.noreply.github.com>
2021-07-11 17:07:57 +0200

    Update search.js

--- js/modules/search.js
+++ js/modules/search.js
@@ -10,23 +10,29 @@ define([
     Search,
     LayerSearchSource,
 ) {
-    const searchState = new Search({
-        view,
-        includeDefaultSources: true,
-        sources: [
-            new LayerSearchSource({
-                layer: vaccineLayer,
-                searchFields: ["Province_State", "STUSPS"],
-                displayField: "Province_State",
-                suggestionTemplate: "{Province_State},{STUSPS}",
-                searchTemplate: "{Province_State},{STUSPS}",
-                placeholder: "Enter state name",
-                minSuggestCharacters: 1,
-                //name: "Counties",
-                exactMatch: false,
-                outFields: ["*"]
-            })
-        ]
-    });
-    exports.search = searchState;
+    const searchState = function (view, vaccineLayer) {
+        // configure custom search
+        const search = new Search({
+            view,
+            includeDefaultSources: true,
+            sources: [
+                new LayerSearchSource({
+                    layer: vaccineLayer,
+                    searchFields: ["Province_State", "STUSPS"],
+                    displayField: "Province_State",
+                    suggestionTemplate: "{Province_State},{STUSPS}",
+                    searchTemplate: "{Province_State},{STUSPS}",
+                    placeholder: "Enter state name",
+                    minSuggestCharacters: 1,
+                    //name: "Counties",
+                    exactMatch: false,
+                    outFields: ["*"]
+                })
+            ]
+        });
+
+        return search;
+    }
+
+    exports.searchState = searchState;
 });
