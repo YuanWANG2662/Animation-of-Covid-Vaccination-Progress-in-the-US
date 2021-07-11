5c09e2d3148271518e772cf2dc5b43917e3d4f13
YuanWANG2662 <82698716+YuanWANG2662@users.noreply.github.com>
2021-07-11 17:07:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,10 @@ require([
   "esri/widgets/Search",
   "esri/widgets/Search/LayerSearchSource",
   "esri/widgets/TimeSlider",
+  "esri/core/watchUtils",
   //import custom modules
   "./js/modules/renderer.js",
+  "./js/modules/search.js",
 ], function (
   // esri modules
   esriConfig,
@@ -28,20 +30,14 @@ require([
   Search,
   LayerSearchSource,
   TimeSlider,
+  watchUtils,
   //custom modules
   renderer,
+  search,
 ) {
+
   // configure the ArcGIS API key
   esriConfig.apiKey = "AAPK4a51d51457ca47989323c81844ad3f32dID99_JGrxrl1acdATUne0knPXdE79jfqrYCVYUzQ_eevAeai9VKIaZpx15lNhkE";
-
-  //create a new layer
-  const vaccineLayer = new FeatureLayer({
-    title: "Covid19 Vaccine Time Series",
-    url: "https://services.arcgis.com/Sf0q24s0oDKgX14j/arcgis/rest/services/covid19_vaccine_time_series_us_state/FeatureServer/0",
-    copyright: "App and maps by <a href=\"https://github.com/YuanWANG2662\">Yuan Wang</a>",
-    outFields: ["*"],
-    renderer: renderer.dotDensityRenderer // imported from the 'renderer' module
-  });
 
   //create a map
   const map = new Map({
@@ -56,6 +52,17 @@ require([
     container: "viewDiv" // viewDiv element
   });
 
+  //get rendererType
+  const rendererSelect = document.getElementById("renderer-select");
+
+  //create a new layer
+  const vaccineLayer = new FeatureLayer({
+    title: "Covid-19 Daily Vaccination in the US",
+    url: "https://services.arcgis.com/Sf0q24s0oDKgX14j/arcgis/rest/services/covid19_vaccine_time_series_us_state/FeatureServer/0",
+    copyright: "App and maps by <a href=\"https://github.com/YuanWANG2662\">Yuan Wang</a>",
+    outFields: ["*"],
+    renderer: renderer.createRenderer(rendererSelect.value) // imported from the 'renderer' module
+  });
 
   // add the layer to the map
   map.add(vaccineLayer);
@@ -87,28 +94,10 @@ require([
     position: "bottom-right"
   });
 
-  // configure custom search
-  const search = new Search({
-    view,
-    includeDefaultSources: true,
-    sources: [
-      new LayerSearchSource({
-        layer: vaccineLayer,
-        searchFields: ["Province_State", "STUSPS"],
-        displayField: "Province_State",
-        suggestionTemplate: "{Province_State},{STUSPS}",
-        placeholder: "Enter state name",
-        minSuggestCharacters: 1,
-        exactMatch: false,
-        outFields: ["*"]
-      })
-    ]
-  });
-
   //add the search button
   view.ui.add(new Expand({
     view,
-    content: search
+    content: search.searchState(view, vaccineLayer) // imported from the 'search' module
   }), "top-right");
 
   // add the ui controls as an expandable icon
@@ -119,20 +108,21 @@ require([
     expanded: true
   }), "top-left");
 
+
+  //add the time-slider-toggle and the timeSlider
   view.ui.add("time-slider-toggle", "bottom-left");
   view.ui.add("timeSliderDiv", "bottom-left");
 
+  //configure the expandable time slider toggle button
   const timeVisibilityBtn = document.getElementById("time-slider-toggle");
   const timeSliderDiv = document.getElementById("timeSliderDiv");
 
+  //when click on the time slider toggle button, the timeSlider switch visibility
   timeVisibilityBtn.addEventListener("click", toggleTimeOptionsVisibility);
-
   function toggleTimeOptionsVisibility() {
-
+    //switch visibility of the timeSlider
     timeSliderDiv.style.visibility = (timeSliderDiv.style.visibility === "hidden") ? "visible" : "hidden";
-
-    //console.log(timeSliderDiv.style.visibility);
-
+    //switch icon style at the same time
     if (timeVisibilityBtn.classList.contains("esri-icon-time-clock")) {
       timeVisibilityBtn.classList.replace("esri-icon-time-clock", "esri-icon-expand");
     } else {
@@ -140,24 +130,25 @@ require([
     }
   }
 
-
-  // configure timeSlider 
+  // set initial time extent
   const initialTimeExtent = {
     start: new Date(2020, 11, 14),  // 2020.12.14 new Date(year, monthIndex, day)
     end: new Date(2021, 6, 3)       //  2021.7.3  new Date(year, monthIndex, day)
   }
+  // configure timeSlider 
   const timeSlider = new TimeSlider({
     container: "timeSliderDiv",
     playRate: 100,
     loop: true,
     viewModel: {
       view: view,
-      mode: "instant",
+      mode: "instant", //instant time value instead of time window
       fullTimeExtent: initialTimeExtent,
+      timeExtent: {
+        start: initialTimeExtent.end,
+        end: initialTimeExtent.end
+      },
     },
-    // fullTimeExtent: initialTimeExtent,
-    // mode: "instant",
-    values: [initialTimeExtent.end],
     stops: {
       interval: {
         value: 1,
@@ -166,27 +157,32 @@ require([
     }
   });
 
-  // timeSlider.on(["thumb-change", "thumb-drag"], (event) => {
-  //   const renderer = vaccineLayer.renderer.clone();
-  //   // update renderer to reference field
-  //   // representing selected year on the slider
-  //   const date = event.value;
-  //   const fieldName = "date_" + date.getMonth() + 1 + "_" + date.getDate() + "_" + date.getFullYear();
-  //   console.log(fieldName);
-  //   renderer.attributes.filed = fieldName;
-  //   vaccineLayer.renderer = renderer;
-  // });
-
+  // When the user changes the value of the time slider, change the renderer to reflect
+  // data corresponding to the date indicated on the slider
 
   timeSlider.watch("values", () => {
-    const renderer = vaccineLayer.renderer.clone();
+    // representing selected date on the slider
+    const activeDate = timeSlider.values[0];
     // update renderer to reference field
-    // representing selected year on the slider
-    const date = timeSlider.values[0];
-    const fieldName = "date_" + ("0" + (date.getMonth() + 1)).slice(-2) + "_" + ("0" + date.getDate()).slice(-2) + "_" + date.getFullYear();
-    console.log(fieldName);
-    renderer.attributes.filed = fieldName;
-    vaccineLayer.renderer = renderer;
+    renderer.updateRenderer(vaccineLayer, activeDate);
   });
+
+  //update the map view when the user changes the value of the time slider
+  view
+    .whenLayerView(vaccineLayer)
+    .then(function (layerView) {
+      watchUtils.whenFalseOnce(layerView, "updating", function () {
+        const activeDate = timeSlider.values[0];
+        renderer.updateRenderer(vaccineLayer, activeDate);
+      });
+    });
+
+  rendererSelect.addEventListener("change", () => {
+    updateLayer(false);
+  });
+
+
+
+
 
 });
